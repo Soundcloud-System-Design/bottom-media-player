f0805d650b2ed8a6f55083171e00dc4ed50a7666
Andrea Sekito <andrea@Chonkers-Computer.local>
2020-04-01 11:30:40 -0700

    function properly on my device with mysql settings

--- server/model/index.js
+++ server/model/index.js
@@ -1,8 +1,8 @@
-var db = require("../database");
+var db = require('../database');
 module.exports = {
   songs: {
     getPlaylist: callback => {
-      var queryString = "select * from songList";
+      var queryString = 'select * from songList';
       // randomize the sqlChart.
       return db.query(queryString, function(err, results) {
         console.log(results);
