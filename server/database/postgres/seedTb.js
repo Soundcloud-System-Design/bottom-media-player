f8483ee005041132f6db177c26134ba4462965bb
Andrea Sekito <andrea@Chonkers-Computer.local>
2020-04-09 15:09:26 -0700

    database initialization completed

--- server/database/postgres/seedTb.js
+++ server/database/postgres/seedTb.js
@@ -1,35 +1,5 @@
 const pool = require("./dbconnect.js");
 
-// pool.query(
-//   "COPY songs (music_title, music_url, cover_art) FROM '/Users/andrea/Desktop/bottom-media-player/server/database/postgres/generatedSongs.csv' DELIMITER ',' CSV HEADER",
-//   (err, result) => {
-//     if (err) {
-//       throw err;
-//     }
-//     console.log("copied songs readdyyyy");
-//     console.log(result);
-//     pool.query(
-//       "COPY playlists (playlist_title) FROM '/Users/andrea/Desktop/bottom-media-player/server/database/postgres/generatedPlaylists.csv' DELIMITER ',' CSV HEADER",
-//       (err, result) => {
-//         if (err) {
-//           throw err;
-//         }
-//         console.log("copied playlists hoe");
-//         pool.query(
-//           "COPY artists (artist_name) FROM '/Users/andrea/Desktop/bottom-media-player/server/database/postgres/generatedArtists.csv' DELIMITER ',' CSV HEADER",
-//           (err, result) => {
-//             if (err) {
-//               throw err;
-//             }
-//             console.log("copied artists hoe");
-//             pool.end();
-//           }
-//         );
-//       }
-//     );
-//   }
-// );
-
 pool
   .query(
     "COPY artists (artist_name) FROM '/Users/andrea/Desktop/bottom-media-player/server/database/postgres/generatedArtists.csv' DELIMITER ',' CSV HEADER"
@@ -57,6 +27,10 @@ pool
     return pool.query(
       "COPY playlist_songs (playlist_id, song_id) FROM '/Users/andrea/Desktop/bottom-media-player/server/database/postgres/generetedSongsAndPlaylists.csv' DELIMITER ',' CSV HEADER"
     );
+  })
+  .then(() => {
+    console.log("indexing");
+    return pool.query("CREATE INDEX ON playlist_songs (playlist_id);");
   })
   .then((result) => {
     console.log(result);
